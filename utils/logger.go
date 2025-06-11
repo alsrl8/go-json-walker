96d302c6f9c0aa3113467265c0681021e4f0ff8d
Ramsy4754 <ramsy4754@clo-guard.com>
2025-06-11 13:26:24 +0900

    Feat: `slog` library logger 추가

---
+++ utils/logger.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"log"
+	"log/slog"
+	"os"
+	"sync"
+)
+
+type Logger interface {
+	Info(msg string, args ...interface{})
+	Debug(msg string, args ...interface{})
+	Warn(msg string, args ...interface{})
+	Error(msg string, args ...interface{})
+}
+
+var (
+	once     sync.Once
+	goLogger *GoLogger
+)
+
+type GoLogger struct {
+	*slog.Logger
+}
+
+func GetLogger() *GoLogger {
+	once.Do(func() {
+		file, err := os.Create("./logs/app.log")
+		if err != nil {
+			log.Fatal(err)
+		}
+		h := slog.NewJSONHandler(
+			file,
+			&slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			},
+		)
+		slogger := slog.New(h)
+		goLogger = &GoLogger{
+			slogger,
+		}
+	})
+	return goLogger
+}
+
+func (gl *GoLogger) Info(msg string, args ...interface{}) {
+	gl.Logger.Info(msg, args...)
+}
+
+func (gl *GoLogger) Debug(msg string, args ...interface{}) {
+	gl.Logger.Debug(msg, args...)
+}
+
+func (gl *GoLogger) Warn(msg string, args ...interface{}) {
+	gl.Logger.Warn(msg, args...)
+}
+
+func (gl *GoLogger) Error(msg string, args ...interface{}) {
+	gl.Logger.Error(msg, args...)
+}
